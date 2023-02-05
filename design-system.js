3e78eeb7b437ddb4e07ceea68335e24e08077558
Pillairenu <113142647+Pillairenu@users.noreply.github.com>
2023-02-04 21:50:00 -0800

    Add Side Navigation

---
+++ design-system.js
@@ -0,0 +1,27 @@
+"use strict"
+
+function Search() {
+  let input, filter, ul, li, a, i;
+  input = document.getElementById("cb-Search");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("nav-menus");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+  ol=document.getElementById("nav-submenu");
+  oi=ol.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+    a = oi[i].getElementsByTagName("a")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      oi[i].style.display = "";
+    } else {
+      oi[i].style.display = "none";
+    }
+  }
+}
