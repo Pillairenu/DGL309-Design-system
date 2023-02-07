e5a117c9e0969a5af9be399965f6d0824f681ab1
Pillairenu <113142647+Pillairenu@users.noreply.github.com>
2023-02-07 13:17:42 -0800

    Add Hamburger menu

--- design-system.js
+++ design-system.js
@@ -25,3 +25,13 @@ function Search() {
     }
   }
 }
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
